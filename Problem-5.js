3dc7fa53dede2bb0e3bbf1a73ba44856905031a4
jubair-jx <jubair.official97@gmail.com>
2023-07-14 01:48:16 +0600

    Fifth Problem-Using Short Condition

---
+++ Problem-5.js
@@ -0,0 +1,8 @@
+//Make any short condition using Short conditonals
+
+const coder = "Jubair";
+
+//make with using &&
+coder === "Jubair" && console.log("I love Jubair");
+
+coder === "Jubair" || console.log("I hate U");
