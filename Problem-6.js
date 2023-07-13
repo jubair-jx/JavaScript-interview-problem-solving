7a9556b1271ddd3c50b15dea0d71f7d610fa9513
jubair-jx <jubair.official97@gmail.com>
2023-07-14 01:56:21 +0600

    Fifth Problem-Replace Some word which is wrong for this sentence

---
+++ Problem-6.js
@@ -0,0 +1,6 @@
+//Replace some word which is wrong
+
+const kotha =
+  "Next JS is a React library & this library is the most popular for SSR library now ";
+
+console.log(kotha.replace(/library/g, "framework"));
